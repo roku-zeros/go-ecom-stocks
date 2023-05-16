9e674b0e8b5a83b0844cbe800707fd61c1cb1ce7
Zerogi <rob.zgonnikov@gmail.com>
2023-05-16 16:33:53 +0300

    initial

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"log"
+	"os"
+
+	"github.com/akhil/ecommerce-yt/controllers"
+	"github.com/akhil/ecommerce-yt/database"
+	"github.com/akhil/ecommerce-yt/middleware"
+	"github.com/akhil/ecommerce-yt/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(cors.Default())
+
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
+}
